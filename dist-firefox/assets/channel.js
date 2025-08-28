d88b755333ee7a75a51fe0da9a790d223198aef5
justrach <54503978+justrach@users.noreply.github.com>
2025-08-28 22:44:01 +0800

    removed dist-firefox

--- dist-firefox/assets/channel.js
+++
@@ -1,8 +0,0 @@
-import { U as Utils, C as Color } from "./mermaid.core.js";
-const channel = (color, channel2) => {
-  return Utils.lang.round(Color.parse(color)[channel2]);
-};
-export {
-  channel as c
-};
-//# sourceMappingURL=channel.js.map
